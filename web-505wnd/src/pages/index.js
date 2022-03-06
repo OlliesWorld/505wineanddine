cf1557a35e60453a4cdff97175fb9c914e238fb4
Roni <rtlockwoodwork@gmail.com>
2022-03-05 22:03:49 -0700

    section titles

--- web-505wnd/src/pages/index.js
+++ web-505wnd/src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = ({data}) => {
       />
       <div className="grid-events ">
       
-      
+      <div className="section-title">Event to have fun with!</div>
        {data && 
           data.event.edges.map(({node: event}) => (
             <div className="event" key={event.id}>
@@ -35,7 +35,7 @@ const IndexPage = ({data}) => {
       <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
     
       <div className="grid-posts">
-      
+      <div className="section-title">Read all about it: Posts</div>
       
       {data && 
          data.post.edges.map(({node: post}) => (
